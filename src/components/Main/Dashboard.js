4813286851e82fb8ecdbf80984cf32a7cb7fd811
AndreyPrudnikoff <a.prudniks@gmail.com>
2021-01-13 22:15:03 +0200

    timer

--- src/components/Main/Dashboard.js
+++ src/components/Main/Dashboard.js
@@ -11,7 +11,7 @@ import {connect} from "react-redux";
 class Dashboard extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {bet: .5, predict: false};
+        this.state = {bet: .5, predict: false, counter: 10};
         this.setBet = this.setBet.bind(this);
         this.predictSubmit = this.predictSubmit.bind(this);
     }
@@ -20,16 +20,21 @@ class Dashboard extends React.Component {
         const newBet = {bet: e.target.value}
         this.setState((state) => ({...state, ...newBet}));
     }
+
     predictSubmit(e) {
         e.preventDefault();
+        const timer = setInterval(() => {
+            this.setState((state) => ({...state, counter: state.counter - 1}));
+        }, 1000)
         this.setState((state) => ({...state, predict: true}));
         return setTimeout(() => {
-            this.setState((state) => ({...state, predict: false}))
-        }, 5000)
+            clearInterval(timer);
+            this.setState((state) => ({...state, predict: false, counter: 10}))
+        }, 10000)
     }
 
     render() {
-        const {bet, predict} = this.state;
+        const {bet, predict, counter} = this.state;
         const {balance} = this.props;
         return (
             <div className="round dashboard">
@@ -87,14 +92,14 @@ class Dashboard extends React.Component {
                                     </label>
                                     <input min="0.001" max="1" step="0.001"
                                            type="range"
+                                           disabled={predict}
                                            style={{backgroundImage: `linear-gradient(to right, ${balance - bet >= 0 ? '#32D74B' : '#FF453A'} 0%, ${balance - bet >= 0 ? '#32D74B' : '#FF453A'} ${bet * 100}%, #fff ${bet * 100}%, white 100%)`}}
                                            onInput={this.setBet}
                                            className={balance - bet >= 0 ? 'green-range' : 'red-range'}
                                            id="range"/>
                                 </div>
                                 {balance - bet >= 0
-                                    ? <>
-                                    </>
+                                    ? <></>
                                     : <>
                                         <p className="btn bet-btn col-sm-4"><span>Not enough</span></p>
                                         <div className='wrap-btn'>
@@ -103,18 +108,24 @@ class Dashboard extends React.Component {
                                             </button>
                                         </div>
                                     </>}
-                            </div>
+
                             {balance - bet >= 0
-                                ? <div className='wrap-btn'>
-                                    <button disabled={predict} onClick={this.predictSubmit} className="btn green predict-btn">PREDICT UP
-                                        <img src={arrowUp} width="15" height="20" alt="b"/>
-                                    </button>
-                                    <button disabled={predict} onClick={this.predictSubmit} className="btn red predict-btn">PREDICT DOWN
-                                        <img src={arrowDown} width="15" height="20" alt="b"/>
-                                    </button>
-                                </div>
+                                ? <>
+                                    <p style={{display: predict ? 'block' : 'none'}} className="btn bet-btn col-sm-4"><span>00:{counter > 9 ? counter : '0' + counter}</span></p>
+                                    <div className='wrap-btn'>
+                                        <button disabled={predict} onClick={this.predictSubmit}
+                                                className="btn green predict-btn">PREDICT UP
+                                            <img src={arrowUp} width="15" height="20" alt="b"/>
+                                        </button>
+                                        <button disabled={predict} onClick={this.predictSubmit}
+                                                className="btn red predict-btn">PREDICT DOWN
+                                            <img src={arrowDown} width="15" height="20" alt="b"/>
+                                        </button>
+                                    </div>
+                                </>
                                 : <>
                                 </>}
+                            </div>
                         </form>
                     </div>
                 </div>
@@ -122,6 +133,7 @@ class Dashboard extends React.Component {
         );
     }
 }
+
 const mapStateToProps = state => {
     return {balance: state.balanceReducer.balance}
 }
