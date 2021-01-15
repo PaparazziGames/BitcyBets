061e132b0257af8cc4e2e4dc2b565852095d5c27
AndreyPrudnikoff <a.prudniks@gmail.com>
2021-01-15 21:31:56 +0200

    fireworks v 1.2

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -37,7 +37,7 @@ const fire = () => {
 const Main = ({course, lastWin, closeCongratulation, congratulation, currentCourse, money}) => {
     useEffect(() => {
         fire();
-    })
+    }, [])
     return (
         <div className="main">
             <div style={{display: congratulation ? "block" : "none"}} className="blur">
